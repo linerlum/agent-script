c73c6b7b5a7dce79c1336d18f3663a9a7d65050d
Emmanuel Bayere <emmanuel.bayere@gmail.com>
2020-04-11 12:53:00 -0400

    merge conflict

--- kernel.sh
+++ kernel.sh
@@ -8,14 +8,11 @@ fi
 lscpu
 lsblk
 uname -r
-<<<<<<< HEAD
 cat /proc/meminfo
 cat /proc/cpuinfo
 lsb_release -a
-=======
 free -m
 iostat
 cat /etc/os-release
 sar -u
 nproc
->>>>>>> 27114142b52b75f9c8225ea24083a619cfd9b6a5
